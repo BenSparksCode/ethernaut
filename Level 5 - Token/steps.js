2965f8b76a4c33d702dab850af077b8f9949dc23
Ben Sparks <bensparks0@gmail.com>
2020-12-19 23:56:13 +0200

    Started Level 5 - Token

---
+++ Level 5 - Token/steps.js
@@ -0,0 +1,11 @@
+// LEVEL GOALS:
+// 1. Hold more than 20 tokens
+// -------------------------------
+// STRATEGY:
+// 1. Something to do with "odometer"? Measuring distance?
+// 2. 
+// -------------------------------------------- 
+// STEPS TO COMPLETE LEVEL
+// -----------------------
+
+// 1 - 
