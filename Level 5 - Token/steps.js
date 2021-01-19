1b5db4e68f95302486ad46bfd30aff4e34eda6c6
Ben Sparks <bensparks0@gmail.com>
2021-01-19 23:24:18 +0200

    Progress on Level 5 - Token

--- Level 5 - Token/steps.js
+++ Level 5 - Token/steps.js
@@ -2,8 +2,11 @@
 // 1. Hold more than 20 tokens
 // -------------------------------
 // STRATEGY:
-// 1. Something to do with "odometer"? Measuring distance?
-// 2. 
+// 1. Odometers tick back to zero once they reach the max num
+// 2. Exploit the transfer() function by transferring a positive
+//      num of tokens from a proxy contract that has 0 tokens
+// 3. Should pass the require() by underflowing
+// 4. Should add a positive token amount to msg.sender and _to
 // -------------------------------------------- 
 // STEPS TO COMPLETE LEVEL
 // -----------------------
