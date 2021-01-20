0b1cff9fc2edf3d256cfb670335efdf800426b2f
Ben Sparks <bensparks0@gmail.com>
2021-01-20 15:09:42 +0200

    Started Level 6 - Delegation

---
+++ Level 6 - Delegation/steps.js
@@ -0,0 +1,10 @@
+// LEVEL GOALS:
+// 1. Claim ownership of contract instance
+// -------------------------------
+// STRATEGY:
+// 1. 
+// -------------------------------------------- 
+// STEPS TO COMPLETE LEVEL
+// -----------------------
+
+// 1 - 
