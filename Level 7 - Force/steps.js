f8820103e752e3fba59a287fc8dd84de288067ae
Ben Sparks <bensparks0@gmail.com>
2021-03-06 13:18:01 +0200

    Started Level 7 - Force

---
+++ Level 7 - Force/steps.js
@@ -0,0 +1,10 @@
+// LEVEL GOALS:
+// 1. Make the balance of the contract greater than zero.
+// -------------------------------
+// STRATEGY:
+// 1.  
+// -------------------------------------------- 
+// STEPS TO COMPLETE LEVEL
+// -----------------------
+
+// 1 - 
