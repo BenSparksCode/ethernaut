b89417d532f0f35f5b302935c96b87ca38fbf99d
Ben Sparks <bensparks0@gmail.com>
2021-03-06 17:24:08 +0200

    Updated strategy explanation in Level 6 - Delegation

--- Level 6 - Delegation/steps.js
+++ Level 6 - Delegation/steps.js
@@ -2,10 +2,15 @@
 // 1. Claim ownership of contract instance
 // -------------------------------
 // STRATEGY:
-// 1. Call pwn() in the Delegate contract, from the
-// Delegation contract, by using delegatecall() in the
-// fallback function and passing in the hash of the pwn()
-// function in the data field of the transaction. 
+// 1. By triggering the Delegation contract to delegatecall the pwn()
+// function in Delegate, we can preserve the msg.sender (and msg.value)
+// values that we originally sent in the function call to the Delegation contract.
+// 2. This means msg.sender will still be our address even though the Delegation
+// contract is calling the Delegate contract's pwn() function.
+// 3. This will set our address as the owner in Delegate.
+// 4. To do this, we need to trigger the fallback function in Delegation,
+// and make it call a pwn() function in Delegate. We do this by passing the
+// hash of the pwn() function in the data field.
 // -------------------------------------------- 
 // STEPS TO COMPLETE LEVEL
 // -----------------------
